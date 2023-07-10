f35ab69860bbece713f3465d0a5740bd3e974a46
Mixchelle <michelle.biologia@hotmail.com>
2023-07-10 09:22:27 -0300

    Iniciando o projeto

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,9 +5,9 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=lambda x: x.count('/'))
         print(f"Deepest file: {deepest_file}")
-
+        
 
 def find_file_by_name(context, search_term, case_sensitive=True):
     if not search_term:
